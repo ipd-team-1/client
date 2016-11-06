746529ade56b9987b04583342ffb69cb346740bb
David Zhu <hdavidzhu@gmail.com>
2016-11-06 01:10:47 -0400

    ADD: Root.

---
+++ src/components/root/index.js
@@ -0,0 +1,9 @@
+require('./index.scss');
+const template = require('./index.html');
+
+module.exports = {
+  name: 'cmp-root',
+  properties: {
+    template,
+  },
+};
--- src/entry.js
+++ src/entry.js
@@ -4,7 +4,8 @@ function componentize(component) {
   Vue.component(component.name, component.properties);
 }
 
-componentize(require('./components/art_display/index.js'));
+componentize(require('./components/root'));
+componentize(require('./components/art_display'));
 
 /* eslint-disable no-new */
 new Vue({
